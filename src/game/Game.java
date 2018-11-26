005137598bdb5ec0c06c0ff5ff47c856d4184f70
doinkboink <alexwei@outlook.com>
2018-11-26 12:56:45 -0800

    Something

--- src/game/Game.java
+++ src/game/Game.java
@@ -9,7 +9,7 @@ public class Game {
 	 * Used to update the game state. Game logic goes here.
 	 */
 	public void update() {
-		
+		//lets do something! ~Alex
 	}
 	public Collection<Renderable> getRenderables(){
 		return null;
