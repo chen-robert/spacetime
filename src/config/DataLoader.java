978fb76790d11e82136a3ceb682b06cfc293b7f2
doinkboink <alexwei@outlook.com>
2018-11-27 09:51:35 -0800

    Added Shipdata and a DataLoader for Robert to work on--
    Please add a test ship along with removing the errors derp

---
+++ src/config/DataLoader.java
@@ -0,0 +1,8 @@
+package config;
+
+public class DataLoader{
+	public static ShipData getShipData(String name){
+		
+		//TODO
+	}
+}
---
+++ src/config/ShipData.java
@@ -0,0 +1,40 @@
+package config;
+
+public interface ShipData {
+	String getName();
+	
+	/**
+	 * @return radius of the circular hitbox
+	 */
+	double getHitboxRadius();
+	/**
+	 * @return a double between 0 and 1, the fraction of energy returned when colliding
+	 */
+	double getRebound();
+	/**
+	 * @return acceleration speed
+	 */
+	double getAcceleration();
+	/**
+	 * @return maximum speed for the ship; on each tick, acceleration vectors are added
+	 * and THEN if they surpass the maximum speed, are scaled down accordingly
+	 */
+	double getMaxSpeed();
+	/**
+	 * @return the number of DEGREES that the ship can turn in either direction each tick
+	 */
+	double getTurnSpeed();
+	
+	/**
+	 * @return number of hitpoints the ship spawns with and can have at max
+	 */
+	int getMaxHP();
+	/**
+	 * @return amount of base damage reduction the ship has (dubious, change/delete as necessary)
+	 */
+	double getDef();
+	/**
+	 * @return coefficient by which damage is scaled by from base weapon damage
+	 */
+	double getDamageMultiplier();
+}
