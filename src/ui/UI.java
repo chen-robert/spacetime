33a2ad1897d47c4b03f2278bc504bebbed03a79f
Robert Chen <robertchen@live.com>
2018-12-10 17:53:49 -0800

    Fixed priority queue rendering order

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -5,7 +5,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.geom.AffineTransform;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.JPanel;
 
@@ -28,10 +29,10 @@ public class UI extends JPanel {
 
 	@Override
 	public void paintComponent(Graphics g) {
-		if (currentFrame == null)
-			return;
+		if (currentFrame == null) return;
 
-		Graphics2D g2d = (Graphics2D) g;// just for safety and more usable features
+		Graphics2D g2d = (Graphics2D) g;// just for safety and more usable
+										// features
 
 		g2d.setColor(BACKGROUND_COLOR);
 		g2d.fillRect(0, 0, getWidth(), getHeight());
@@ -39,8 +40,9 @@ public class UI extends JPanel {
 		g2d.setColor(Color.WHITE);
 		g2d.fillRect(UI.SIDE_BUFFER, UI.SIDE_BUFFER + UI.TOP_BANNER, UI.FIELD_WIDTH, UI.FIELD_HEIGHT);
 
-		PriorityQueue<Renderable> items = new PriorityQueue<>((a, b) -> a.getRenderPriority() - b.getRenderPriority());
-		items.addAll(currentFrame.getRenderables());
+		ArrayList<Renderable> items = new ArrayList<>(currentFrame.getRenderables());
+		Collections.sort(items, (a, b) -> a.getRenderPriority() - b.getRenderPriority());
+
 		for (Renderable item : items) {
 			Image sprite = item.getImg();
 
